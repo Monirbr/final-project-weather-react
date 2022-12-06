6da6f4a38d3f1f3d26b26d6c74cb004fe26a72f7
monirbr <monire.borumand@gmail.com>
2022-12-06 14:26:58 +0330

    styled github link

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="App">
-      <Weather city="tehran" />
-      <a>open-Sourced on GitHub</a>
+    <div className='section' >
+      <div className="App">
+        <Weather city="tehran" />
+      </div>
+      <div className='link'>
+        <a
+          href="https://github.com/Monirbr/final-project-weather-react"
+          target="_blank"
+        >
+          open-Sourced on GitHub
+        </a>
+      </div>
     </div>
-
   );
 }
 
