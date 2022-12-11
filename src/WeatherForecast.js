770a3c8ca6bfafa5f7cdc43f0971ac3417726508
monirbr <monire.borumand@gmail.com>
2022-12-11 12:11:43 +0330

    forecast one day added

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,59 +1,67 @@
 import axios from "axios";
-import React from "react";
+import React,{useState} from "react";
 import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props){
+  let [loaded , setLoaded] = useState(false);
+  let[forecast, setForecast] = useState(null);
 function responseHandler(response){
-  console.log(response);
+  setForecast(response.data.daily)
+  setLoaded(true);
 }
 
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.name}&key=feco5a36c3at9d80680544db0770c40f&units=metric`;
-  axios.get(apiUrl).then(responseHandler);  
-  return (
-      <div classname="forecast-temp">
-        <hr/>
-        <div className="forecast">
-          <div className="col1">
-            <p className="forecast-day">Tue</p>
-            <img className="forecast-icon"  src={props.data.icon} />
-          <div classname="forecast-temp">
-            <span className="max">{Math.round(props.data.temperature)}°</span>
-            <span className="min">{Math.round(props.data.temperature)}°</span>
-          </div>
-          </div>
-          <div className="col1">
-            <p className="forecast-day">Tue</p>
-            <img className="forecast-icon" src={props.data.icon} />
-            <div classname="forecast-temp">
-            <span className="max">{Math.round(props.data.temperature)}°</span>
-            <span className="min">{Math.round(props.data.temperature)}°</span>
-          </div>
-          </div>
-          <div className="col1">
-            <p className="forecast-day">Tue</p>
-            <img className="forecast-icon" src={props.data.icon} />
+
+if(loaded){
+ console.log(forecast)
+   return (
+     <div classname="forecast-temp">
+       <hr />
+       <div className="forecast">
+         <div className="col1">
+          <WeatherForecastDay data = {forecast[0]} />
+           </div>
+         <div className="col1">
+           <p className="forecast-day">Tue</p>
+           <img className="forecast-icon" src={props.data.icon} />
            <div classname="forecast-temp">
-            <span className="max">{Math.round(props.data.temperature)}°</span>
-            <span className="min">{Math.round(props.data.temperature)}°</span>
-          </div>
-          </div>
-          <div className="col1">
-            <p className="forecast-day">Tue</p>
-            <img className="forecast-icon" src={props.data.icon} />
-          <div classname="forecast-temp">
-            <span className="max">{Math.round(props.data.temperature)}°</span>
-            <span className="min">{Math.round(props.data.temperature)}°</span>
-          </div>
-          </div>
-          <div className="col1">
-            <p className="forecast-day">Tue</p>
-            <img className="forecast-icon" src={props.data.icon} />
-          <div classname="forecast-temp">
-            <span className="max">{Math.round(props.data.temperature)}°</span>
-            <span className="min">{Math.round(props.data.temperature)}°</span>
-          </div>
-          </div>
-        </div>
-      </div>
-    );
+             <span className="max">{Math.round(props.data.temperature)}°</span>
+             <span className="min">{Math.round(props.data.temperature)}°</span>
+           </div>
+         </div>
+         <div className="col1">
+           <p className="forecast-day">Tue</p>
+           <img className="forecast-icon" src={props.data.icon} />
+           <div classname="forecast-temp">
+             <span className="max">{Math.round(props.data.temperature)}°</span>
+             <span className="min">{Math.round(props.data.temperature)}°</span>
+           </div>
+         </div>
+         <div className="col1">
+           <p className="forecast-day">Tue</p>
+           <img className="forecast-icon" src={props.data.icon} />
+           <div classname="forecast-temp">
+             <span className="max">{Math.round(props.data.temperature)}°</span>
+             <span className="min">{Math.round(props.data.temperature)}°</span>
+           </div>
+         </div>
+         <div className="col1">
+           <p className="forecast-day">Tue</p>
+           <img className="forecast-icon" src={props.data.icon} />
+           <div classname="forecast-temp">
+             <span className="max">{Math.round(props.data.temperature)}°</span>
+             <span className="min">{Math.round(props.data.temperature)}°</span>
+           </div>
+         </div>
+       </div>
+     </div>
+   );
+    
+
+}else{
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.name}&key=feco5a36c3at9d80680544db0770c40f&units=metric`;
+  axios.get(apiUrl).then(responseHandler);
+  return null;
+ 
+}
 }
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+
+export default function WeatherForecastDay(props){
+    function maxTemp(){
+        let temperature = Math.round(props.data.temperature.maximum);
+        return  `${temperature}°`;
+    }
+    function minTemp() {
+      let temperature = Math.round(props.data.temperature.minimum);
+      return `${temperature}°`;
+    }
+
+    function day(){
+        let date = new Date(props.data.time * 1000);
+        let day = date.getDay();
+
+        let days = ["Sun", "Mon" , " Tue" , "Wed", "Thu" , "Sat"];
+        return days[day];
+    }
+    return (
+      <div>
+        <p className="forecast-day">{day()}</p>
+        <img className="forecast-icon" src={props.data.condition.icon_url} />
+        <div classname="forecast-temp">
+          <span className="max">
+            {maxTemp()}
+          </span>
+          <span className="min">
+            {minTemp()}
+          </span>
+        </div>
+      </div>
+    );
+}
